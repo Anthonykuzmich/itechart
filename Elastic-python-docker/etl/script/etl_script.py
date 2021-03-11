cedf7b6f1fea28100532e43b311a77d7a6a04989
anton <anton@wsa-173-72a.icx.local>
2021-03-11 15:43:33 +0300

    making corrections in data

--- Elastic-python-docker/etl/script/etl_script.py
+++ Elastic-python-docker/etl/script/etl_script.py
@@ -1,40 +1,78 @@
 import sqlite3
 import time
-
+import string
 from elasticsearch import Elasticsearch
+import json
 
-conn = sqlite3.connect('script/dbmovies.sqlite')
+host = [{"host": 'elasticsearch'}]
+
+conn = sqlite3.connect('db.sqlite')
 cur = conn.cursor()
 
-es = Elasticsearch(hosts=[{"host": 'elasticsearch'}], retry_on_timeout=True)
-for _ in range(100):
-    try:
-        # make sure the cluster is available
-        es.cluster.health(wait_for_status='yellow')
-    except ConnectionError:
-        time.sleep(2)
 
-cur.execute("DELETE FROM actors WHERE name='N/A'")
-cur.execute("DELETE FROM writers WHERE name='N/A'")
-cur.execute("UPDATE movies SET director = 'None' WHERE director = 'N/A'")
-data = cur.execute('''
-SELECT m.id, group_concat(a.name) actors_name, m.title, m.genre, m.director, w.name writer_name from actors a
-INNER JOIN movie_actors ma on a.id = ma.actor_id
-INNER JOIN movies m on m.id = ma.movie_id
-INNER JOIN writers w on m.writer = w.id
-GROUP BY m.id;
-''').fetchall()
-names = list(map(lambda x: x[0], cur.description))
-for row in data:
-    names_with_values = dict(zip(names, row))
-    doc = {
-        names[0]: names_with_values['id'],
-        names[1]: names_with_values['actors_name'],
-        names[2]: names_with_values['title'],
-        names[3]: names_with_values['genre'],
-        names[4]: names_with_values['director'],
-        names[5]: names_with_values['writer_name']
-    }
-    res = es.index(index='movies', id=names_with_values['id'], body=doc)
-conn.commit()
-conn.close()
+def clear_data_table():
+    cur.execute("delete from actors where name='N/A'")
+    cur.execute("delete from writers where name='N/A'")
+    cur.execute("update movies set director = null where director = 'None'")
+    cur.execute("update movies set plot = null where plot = 'None'")
+    cur.execute("update movies set imdb_rating = '0' where imdb_rating='N/A'")
+
+
+def data_to_es():
+    data = cur.execute('''
+    with x as (SELECT m.id, group_concat(a.id) as actors_ids, group_concat(a.name) as actors_names
+    FROM movies m
+    LEFT JOIN movie_actors ma on m.id = ma.movie_id
+    LEFT JOIN actors a on ma.actor_id = a.id
+    GROUP BY m.id)
+    
+    
+    SELECT m.id, genre, director, title, plot, imdb_rating, x.actors_ids, x.actors_names,
+    CASE
+    WHEN m.writers = '' THEN '[{"id": "' || m.writer || '"}]'
+    ELSE m.writers
+    END AS writers
+    FROM movies m
+    LEFT JOIN x ON m.id = x.id
+    ''').fetchall()
+
+    return data
+
+
+def migrating_to_es():
+    names_of_colums = list(map(lambda x: x[0], cur.description))
+    for row in data_to_es():
+        names_with_values = dict(zip(names_of_colums, row))
+
+        doc = {
+            names_of_colums[0]: names_with_values['id'],
+            names_of_colums[1]: names_with_values['genre'],
+            names_of_colums[2]: names_with_values['director'],
+            names_of_colums[3]: names_with_values['title'],
+            names_of_colums[4]: names_with_values['plot'],
+            names_of_colums[5]: names_with_values['imdb_rating'],
+            names_of_colums[6]: names_with_values['actors_ids'],
+            names_of_colums[7]: names_with_values['actors_names'],
+            names_of_colums[8]: names_with_values['writers']
+        }
+        writers = json.loads(names_with_values['writers'])
+        for writer in writers:
+            print(writer['id'])
+        res = es.index(index='movies', id=names_with_values['id'], body=doc)
+
+
+if __name__ == "__main__":
+    es = Elasticsearch()
+    for _ in range(100):
+        try:
+            # make sure the cluster is available
+            es.cluster.health(wait_for_status='yellow')
+        except ConnectionError:
+            time.sleep(2)
+
+    clear_data_table()
+    data_to_es()
+    # writers()
+    migrating_to_es()
+    conn.commit()
+    conn.close()
